0e84e83184b5935840c10a6cc04bdc1458323bd2
Hussain Jawaid <hussainjawaid.work@gmail.com>
2025-10-16 12:04:34 +0500

    Add settings class for configurable game parameters and update game initialization to use settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,6 @@
-from ast import main
 import sys
 import pygame
+from settings import Settings
 
 
 class AlienInvasion:
@@ -9,7 +9,9 @@ class AlienInvasion:
         """Initialize the game, and create game resources."""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1024, 600))
+        self.settings = Settings()
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
@@ -20,8 +22,12 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+            
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+            self.clock.tick(60)
 
 
 if __name__ == '__main__':
