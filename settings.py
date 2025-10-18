5df600567b2355217f5972322d0dfef29a3a5952
Hussain Jawaid <hussainjawaid.work@gmail.com>
2025-10-18 10:59:55 +0500

    implemented shooting bullets functionality

--- settings.py
+++ settings.py
@@ -14,3 +14,10 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (255, 215, 0)
+        self.bullets_allowed = 3
